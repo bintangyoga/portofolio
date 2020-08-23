253628b7d6ed4f7e9e96957b3f85f8ed93c07e55
bintangyoga <bintang.parmo@gmail.com>
2020-08-23 20:53:36 +0700

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <h1>About</h1>
+    <NuxtLink to="/">
+      About (internal link that belongs to the Nuxt App)
+    </NuxtLink>
+    <a href="https://nuxtjs.org">External Link to another page</a>
+  </main>
+</template>
+
+<style lang="scss">
+    h1{
+        font-size: 100px;
+    }
+</style>
