1b9962b16b058e7caecad3460a469863eaa8bac8
bintangyoga <bintang.parmo@gmail.com>
2020-08-24 22:08:28 +0700

    add custom font

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,106 @@
-module.exports = {
-    module: {
-      rules: [
-        // ... other rules omitted
-  
-        // this will apply to both plain `.scss` files
-        // AND `<style lang="scss">` blocks in `.vue` files
-        {
-          test: /\.scss$/,
-          use: [
-            'vue-style-loader',
-            'css-loader',
-            'sass-loader'
-          ]
-        }
+
+export default {
+    /*
+    ** Nuxt rendering mode
+    ** See https://nuxtjs.org/api/configuration-mode
+    */
+    mode: 'spa',
+    /*
+    ** Nuxt target
+    ** See https://nuxtjs.org/api/configuration-target
+    */
+    target: 'static',
+    /*
+    ** Headers of the page
+    ** See https://nuxtjs.org/api/configuration-head
+    */
+    head: {
+      title: process.env.npm_package_name || '',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+        { hid: 'title', name: 'title', content: 'Nuxt.js project' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.3/css/bootstrap.min.css' },
+        { rel: 'stylesheet', type: 'text/css', href: 'href="https://fonts.googleapis.com/css2?family=Poppins&display=swap' },
+        { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.7/css/swiper.css' }
       ]
     },
-    // plugin omitted
+    /*
+    ** Global CSS
+    */
+    css: [
+      { src: '~/assets/scss/main.scss', lang: 'scss' }
+    ],
+    /*
+    ** Plugins to load before mounting the App
+    ** https://nuxtjs.org/guide/plugins
+    */
+    plugins: [
+    ],
+    /*
+    ** Auto import components
+    ** See https://nuxtjs.org/api/configuration-components
+    */
+    components: true,
+    /*
+    ** Nuxt.js dev-modules
+    */
+    buildModules: [
+    ],
+    /*
+    ** Nuxt.js modules
+    */
+    modules: [
+      // Doc: https://bootstrap-vue.js.org
+      'bootstrap-vue/nuxt',
+    ],
+    /*
+    ** Build configuration
+    ** See https://nuxtjs.org/api/configuration-build/
+    */
+    build: {
+      
+      /*
+      ** Run ESLint on save
+      */
+      extend (config, ctx) {
+        if (ctx.isDev && ctx.client) {
+          config.module.rules.push({
+            enforce: 'pre',
+            test: /\.(js|vue)$/,
+            loader: 'eslint-loader',
+            exclude: /(node_modules)/
+          })
+  
+          const vueLoader = config.module.rules.find(
+            ({loader}) => loader === 'vue-loader')
+          const { options: {loaders} } = vueLoader || { options: {} }
+          if (loaders) {
+            for (const loader of Object.values(loaders)) {
+              changeLoaderOptions(Array.isArray(loader) ? loader : [loader])
+            }
+          }
+          config.module.rules.forEach(rule => changeLoaderOptions(rule.use))
+          // console.log(util.inspect(config.module.rules, { depth: 6 }))
+        }
+      }
+    }
   }
+  
+  function changeLoaderOptions (loaders) {
+    if (loaders) {
+      for (const loader of loaders) {
+        if (loader.loader === 'sass-loader') {
+          Object.assign(loader.options, {
+            includePaths: ['./assets'],
+            // data: '@import "_imports";'
+          })
+        }
+      }
+    }
+  }
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,40 @@
 <template>
   <main>
-    <h1>Home page</h1>
-    <NuxtLink to="/about">
-      About (internal link that belongs to the Nuxt App)
-    </NuxtLink>
-    <a href="https://nuxtjs.org">External Link to another page</a>
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-md-12 text-center px-5 mt-5">
+          <div>
+            <img src="~/assets/images/image.jpg" class="profile img-fluid rounded-circle" alt="Responsive image" />
+            <h5 class="mt-3">Bintang Yoga Pamungkas</h5>
+            <p>UI/UX Designer</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus beatae expedita obcaecati optio nihil ratione consectetur ipsam impedit iure asperiores. Labore esse corporis quas aliquid voluptatibus porro quo nam impedit.</p>
+          </div>
+          <div>
+            <a href="#" class="btn btn-primary">Email Me</a>
+            <a href="#">Download CV</a>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 card mt-5">
+          <NuxtLink to="/about">
+            <img src="~/assets/images/image.jpg" class="img-fluid" alt="Responsive image" />
+          </NuxtLink>
+        </div>
+        <div class="col-md-6 card">
+          <img src="~/assets/images/image.jpg" class="img-fluid" alt="Responsive image" />
+        </div>
+        <div class="col-md-6 card">
+          <img src="~/assets/images/image.jpg" class="img-fluid" alt="Responsive image" />
+        </div>
+      </div>
+      <div class="row mt-5 flex justify-align-center">
+        <div class="col-md-8 flex flex-row justify-align-center">
+          <img src="~/assets/images/image.jpg" class="logo">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+          <a href="#" class="btn btn-primary">Email Me</a>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
